05c12fcd88703974fce26a95856a37c3c967f3e9
Krzysztof Lapinski <kla2@tdc.dk>
2017-05-17 14:05:19 +0200

    Solution

---
+++ Oceanic/Oceanic/Oceanic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Oceanic.Startup))]
+namespace Oceanic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
