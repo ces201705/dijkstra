b821e051fa96647aef366ed0de0916173e3ddb51
Krzysztof Lapinski <kla2@tdc.dk>
2017-05-18 13:26:49 +0200

    structure change

---
+++ Oceanic/Oceanic/Oceanic.DAL/Models/SegmentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oceanic.Models
+{
+    public class SegmentModel
+    {
+        public string SourceLocationName { get; set; }
+        public string EndLocationName { get; set; }
+        public Decimal Time { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
