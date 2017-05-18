70e0a41fa24aba98f9573940d61afd7e97792bb2
Jacek Rogala <jro@netcompany.com>
2017-05-18 11:47:47 +0200

    Search View initialize

---
+++ Oceanic/Oceanic/Oceanic/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oceanic.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
